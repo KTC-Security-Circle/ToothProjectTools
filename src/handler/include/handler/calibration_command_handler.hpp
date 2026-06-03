a536c74a98ffd02a4d2c526a320c198a879692db
Jo <jo.yasuda.dev@gmail.com>
2026-06-03 04:22:48 +0000

    add: dispatchの実装・Commandの割り当てをhandlerサブライブラリに分割

---
+++ src/handler/include/handler/calibration_command_handler.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "cmd/commands.hpp"
+
+namespace runtime
+{
+struct AppContext;
+}
+
+namespace win
+{
+class Window;
+}
+
+namespace handler::calibration
+{
+
+bool handle(runtime::AppContext& ctx, win::Window& target_window, const cmd::Command& command);
+
+} // namespace handler::calibration
